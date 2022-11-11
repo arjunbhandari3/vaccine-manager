4c315946a39e63152b04d768377da1ba6e550a8c
Arjun Bhandari <arjunbhandari3773@gmail.com>
2022-11-11 16:15:48 +0545

    BE: Auth service Test added

---
+++ server/test/services/auth.test.js
@@ -0,0 +1,57 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import User from '../../src/models/user';
+import * as authUtils from '../../src/utils/auth';
+import * as authService from '../../src/services/auth';
+
+const authResponse = {
+  accessToken: 'accessToken',
+  refreshToken: 'refreshToken',
+  user: {
+    id: 1,
+    email: 'hello@gmail.com',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+};
+
+const signUpResponse = {
+  id: 1,
+  email: 'hello@gmail.com',
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+
+const authData = {
+  email: 'hello@gmail.com',
+  password: 'hello123',
+};
+
+describe('Auth Service tests', () => {
+  it('should sign up user', async () => {
+    sinon.stub(User, 'createUser').returns(signUpResponse);
+
+    const response = await authService.signUp(authData);
+
+    expect(response).to.be.an('object');
+    expect(response.user).to.be.an('object');
+    expect(response.user.id).to.be.an('number');
+    expect(response.user.password).to.be.undefined;
+  });
+
+  it('should sign in user', async () => {
+    sinon.stub(User, 'getUserByEmail').returns(authResponse.user);
+    sinon.stub(authUtils, 'compareHash').returns(true);
+    sinon.stub(authUtils, 'getSignedTokens').returns(authResponse);
+
+    const response = await authService.signIn(authData);
+
+    expect(response).to.be.an('object');
+    expect(response.accessToken).to.be.an('string');
+    expect(response.refreshToken).to.be.an('string');
+    expect(response.user).to.be.an('object');
+    expect(response.user.id).to.be.an('number');
+    expect(response.user.password).to.be.undefined;
+  });
+});
